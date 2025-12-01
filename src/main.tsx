af1db934a06cc09539b2c90c60edd7f10b0d5ce2
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 13:55:19 +0000

    Add src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import { BrowserRouter } from 'react-router-dom'; 
+import App from './App'; 
+import './index.css'; 
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root'); 
+if (rootElement) { 
+  const root = ReactDOM.createRoot(rootElement); 
+  root.render( 
+    <React.StrictMode> 
+      <BrowserRouter basename={import.meta.env.BASE_URL}> 
+        <App /> 
+      </BrowserRouter> 
+    </React.StrictMode> 
+  ); 
+}
